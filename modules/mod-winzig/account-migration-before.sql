a233b76f874ee2f009143ccbd05e45210bc8dc28
Jackson Williams <blot0j@gmail.com>
2022-09-29 03:37:26 +0000

    use FIRST not BEFORE in adding column. added where for safty in adding account id's

--- modules/mod-winzig/account-migration-before.sql
+++ modules/mod-winzig/account-migration-before.sql
@@ -2,12 +2,12 @@
 START TRANSACTION;
 
 -- ADD NEW COLUMN
-ALTER TABLE `reagent_bank` ADD COLUMN `account_id` INT NOT NULL BEFORE `character_id`;
+ALTER TABLE `reagent_bank` ADD COLUMN `account_id` INT NOT NULL FIRST;
 
 -- UPDATE reagent_bank add account ids
 UPDATE `reagent_bank` rb
-INNER JOIN `characters` c 
-ON rb.`character_id` = c.`guid` 
-SET rb.`account_id` = c.`account`;
+INNER JOIN `characters` c ON rb.`character_id` = c.`guid`
+SET rb.`account_id` = c.`account`
+WHERE rb.`account_id` = 0;
 
 COMMIT;
